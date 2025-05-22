4100a6f1a59cab8ecca3649f53ac9136e2b1c97e
DuongVietDuy <duydv.forwork@gmail.com>
2025-05-22 21:53:51 +0700

    update App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,30 @@
-function App() {
+
+import React, { Fragment } from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Default from "./components/Default";
+import { routes } from "./routers/index";
+
+const App = () => {
   return (
     <div className="App">
-      <h1>KyoMatcha</h1>
+      <Router>
+        <Routes>
+          {routes.map((route) => {
+            const Layout = route.isShowHeader ? Default : Fragment;
+            return (
+              <Route
+                key={route.path}
+                path={route.path}
+                element={
+                  <Layout>
+                    <route.page />
+                  </Layout>
+                }
+              />
+            );
+          })}
+        </Routes>
+      </Router>
     </div>
   );
 }
