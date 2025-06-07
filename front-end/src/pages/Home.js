710f7646ec185f1557cc61f517ffd22364806fb2
zanhduc <155509842+zanhduc@users.noreply.github.com>
2025-06-07 14:28:55 +0700

    Update Home.js

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -9,7 +9,7 @@ import { useNavigate } from "react-router-dom";
 import { toast } from 'react-toastify';
 import { useAppContext } from '../context/AppContext';
 
-const BACKEND_URL = "http://localhost:9999";
+const BACKEND_URL = {process.env.REACT_APP_GOOGLE_CLIENT_ID};
 
 const BannerWrapper = styled.div`
   margin-top: 100px;
