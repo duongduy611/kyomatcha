609488885dad14b88fb8ae9dfbf76e23c535c62a
DuongVietDuy <duydv.forwork@gmail.com>
2025-06-08 19:54:56 +0700

    cap nhat home, header

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -354,10 +354,10 @@ const TeaCollection = () => {
                   <ProductPrice>{product.price.toLocaleString('vi-VN')}₫</ProductPrice>
                   <Button
                     className="add-to-cart"
-                   onClick={(e) => {
-    e.preventDefault(); // Ngăn hành vi mặc định nếu cần
-    navigate(`/products/${product.slug}`);
-  }}
+                    onClick={(e) => {
+                            e.preventDefault();
+                            handleAddToCart(product._id);
+                          }}
                   >
                     <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
                       <path d="M9 20a1 1 0 1 0 0 2 1 1 0 0 0 0-2zM19 20a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
